8d88e56f52a2b3bb063e00f369efef028b0529dd
Igor Lima <igor.santos@climatempo.com.br>
2018-05-06 03:16:39 -0300

    bot funcional ao alertar alongamentos

--- slackbot/core.py
+++ slackbot/core.py
@@ -1,120 +1,145 @@
-from datetime import datetime, timedelta
-import os
-import time
+# instantiate Slack client
 import re
+import threading
+import time
+import string
+from random import *
 from slackclient import SlackClient
-import pandas
-from slacker import Slacker
+from datetime import datetime
+from pymongo import MongoClient
 
-TOKEN = 'xoxb-358320739216-RCh6mPUo0mwQlzRngFqfPdqS'
-# def parse_bot_commands(slack_events):
-#     """
-#         Parses a list of events coming from the Slack RTM API to find bot commands.
-#         If a bot command is found, this function returns a tuple of command and channel.
-#         If its not found, then this function returns None, None.
-#     """
-#     for event in slack_events:
-#         if event["type"] == "message" and not "subtype" in event:
-#             user_id, message = parse_direct_mention(event["text"])
-#             if user_id == starterbot_id:
-#                 return message, event["channel"]
-#     return None, None
-#
-# def parse_direct_mention(message_text):
-#     """
-#         Finds a direct mention (a mention that is at the beginning) in message text
-#         and returns the user ID which was mentioned. If there is no direct mention, returns None
-#     """
-#     matches = re.search(MENTION_REGEX, message_text)
-#     # the first group contains the username, the second group contains the remaining message
-#     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
-#
-# def handle_command(command, channel):
-#     """
-#         Executes bot command if the command is known
-#     """
-#     # Default response is help text for the user
-#     default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
-#
-#     # Finds and executes the given command, filling in response
-#     response = None
-#     # This is where you start to implement more commands!
-#     if command.startswith(EXAMPLE_COMMAND):
-#         response = "Sure...write some more code then I can do that!"
-#
-#
-#     # Sends the response back to the channel
-#     slack_client.api_call(
-#         "chat.postMessage",
-#         channel=channel,
-#         text=response or default_response
-#     )
-#
-#
-# # instantiate Slack client
-# slack_client = SlackClient(TOKEN)
-# # starterbot's user ID in Slack: value is assigned after the bot starts up
-# starterbot_id = None
-#
-# # constants
-# RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-# EXAMPLE_COMMAND = "do"
-# MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
-#
-#
-#
-# _times = pandas.date_range(datetime.now(), datetime.now() + timedelta(hours=4), freq='1min')
-# if __name__ == "__main__":
-#     if slack_client.rtm_connect(with_team_state=False):
-#         print("Starter Bot connected and running!")
-#         # # Read bot's user ID by calling Web API method `auth.test`
-#         starterbot_id = slack_client.api_call("auth.test")["user_id"]
-#         command, channel = parse_bot_commands(slack_client.rtm_read())
-#         # while True:
-#         #     command, channel = parse_bot_commands(slack_client.rtm_read())
-#         #     if command:
-#         #         handle_command(command, channel)
-#         #     time.sleep(RTM_READ_DELAY)
-#         for _time in _times:
-#             slack_client.api_call(
-#                 "chat.postMessage",
-#                 channel=channel,
-#                 text='Texto enviado  {data:%H:%M:%S}'.format(_time)
-#             )
-#
-#
-#
-#     else:
-#         print("Connection failed. Exception traceback printed above.")
-moreInfo = ['person', 'person2', 'person3']
+mongoClient = MongoClient('mongodb+srv://codeinpolar:codeinpolar@codeinpolar-quc9o.mongodb.net/test')
+db = mongoClient.get_database('codeinquero')
+exercicios = db.get_collection('exercicios')
+
+slack_client = SlackClient('xoxb-358320739216-Ua04LCfZnkz7MzAv8PKXnbJQ')
+# starterbot's user ID in Slack: value is assigned after the bot starts up
+starterbot_id = None
+global id
+id = None
+
+# constants
+RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
+EXAMPLE_COMMAND = "do"
+MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
+
+valid_response = ['sim','não','nao']
+
 message = [{
-    "title": "Hora de se alongar!",
-    "text": "Link:",
-    "actions": [
-        {
-            "name": "yes",
-            "text": "Sim",
-            "style": "primary",
-            "type": "button",
-            "value": True
-        },
-        {
-            "name": "no",
-            "text": "Não",
-            "style":"danger",
-            "type": "button",
-            "value": False
-        },
-        {
-            "name": "donot",
-            "style":"warning",
-            "text": "Não enviar mais",
-            "type": "button",
-            "value": None
-        }
-    ]
+    "title": "Hora de se alongar! Responda com sim ou não para sabermos se realizou a atividade!",
+    "text": "Link: ",
+
 
 }]
-slackClient = Slacker("xoxb-358799440644-VUbS2MgtmnsSFkuUVQCyyPKG")
-slackClient.chat.post_message(channel="#teste", attachments=message)
-response = slackClient.users.list()
+
+def generate_id():
+
+    min_char = 8
+    max_char = 12
+    allchar = string.ascii_letters + string.punctuation + string.digits
+    id = "".join(choice(allchar) for x in range(randint(min_char, max_char)))
+    return id
+
+
+def parse_bot_commands(slack_events,id):
+
+
+    """
+        Parses a list of events coming from the Slack RTM API to find bot commands.
+        If a bot command is found, this function returns a tuple of command and channel.
+        If its not found, then this function returns None, None.
+    """
+    for event in slack_events:
+        if event["type"] == "message":
+            print(event)
+
+            if 'username' in event and event['username'] != 'codeinpolar': continue
+
+            elif 'user' in event:
+                message = event['text'].lower()
+                user = event['user']
+                tmp = {}
+                tmp['id'] = id
+                tmp['user'] = user
+
+                tmp['datetime'] = datetime.utcnow()
+                if message in valid_response:
+                    if 'sim' in message:
+                        tmp['exercicios'] = True
+
+
+                    else:
+                        tmp['exercicios'] = False
+                    exercicios.insert(tmp)
+                else:
+                    slack_client.api_call(
+                        "chat.postMessage",
+                        channel=event['channel'],
+                        text='Resposta inválida, vá descansar!'
+                    )
+
+
+
+
+    return None, None
+
+def parse_direct_mention(message_text):
+    """
+        Finds a direct mention (a mention that is at the beginning) in message text
+        and returns the user ID which was mentioned. If there is no direct mention, returns None
+    """
+    matches = re.search(MENTION_REGEX, message_text)
+    # the first group contains the username, the second group contains the remaining message
+    return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
+
+def handle_command(command, channel,id):
+    """
+        Executes bot command if the command is known
+    """
+    # Default response is help text for the user
+    default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
+
+    # Finds and executes the given command, filling in response
+    response = None
+    # This is where you start to implement more commands!
+    if command.startswith(EXAMPLE_COMMAND):
+        response = "Sure...write some more code then I can do that!"
+
+    # Sends the response back to the channel
+    slack_client.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=response or default_response
+    )
+
+def send_alongamento_message():
+    id = generate_id()
+    slack_client.api_call(
+        "chat.postMessage",
+        channel='teste',
+        text='hora de se alongar!',
+        attachments=message
+    )
+
+if __name__ == "__main__":
+    if slack_client.rtm_connect(with_team_state=False):
+        print("Starter Bot connected and running!")
+        # Read bot's user ID by calling Web API method `auth.test`
+        starterbot_id = slack_client.api_call("auth.test")["user_id"]
+        threading.Timer(600.0, send_alongamento_message).start()
+        id = send_alongamento_message()
+        while True:
+
+            command, channel = parse_bot_commands(slack_client.rtm_read(),id)
+            if command:
+                handle_command(command, channel)
+            time.sleep(RTM_READ_DELAY)
+
+
+
+    else:
+        print("Connection failed. Exception traceback printed above.")
+
+
+
+
