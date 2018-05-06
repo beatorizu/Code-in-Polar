7f6cdd81efe2ec419ee97c0b0b5519bd52e7a9f6
beatorizu <betrizaf@gmail.com>
2018-05-05 22:04:36 -0300

    Adicionada view da home

--- restapi/restapi/urls.py
+++ restapi/restapi/urls.py
@@ -17,7 +17,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
+from dashboard import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^$', views.home)
 ]
